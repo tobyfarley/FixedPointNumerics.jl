ff85021a104a2f323f93a35c196fa29daa03ad6b
Toby Farley <tobyfarley@gmail.com>
2025-04-28 15:45:00 -0400

    Created new constructor for BigFixedPoint

--- src/BigFixedPoint.jl
+++ src/BigFixedPoint.jl
@@ -68,6 +68,13 @@ function BigFixedPoint(v::V, p::P) where {V<:AbstractFloat, P<:Integer}
     BigFixedPoint{BigInt,P}(Int(trunc(round(v; digits=p) * (10^p))), Int(p))
 end
 
+function BigFixedPoint(v::V, p::P) where {V<:BigFloat, P<:Integer}
+    if (v*(10^p)) > maxintfloat(typeof(v))
+        throw(InexactError(BigFixedPoint, typeof(v), v))
+    end    
+    BigFixedPoint{BigInt,P}(Int(trunc(round(v; digits=p) * (10^p))), Int(p))
+end
+
 function string(z::BigFixedPoint; base::Integer = 10, pad::Integer = 1)
     if base ≠ 10
         prinln("Warning: Bases other than 10 not yet implemented")
