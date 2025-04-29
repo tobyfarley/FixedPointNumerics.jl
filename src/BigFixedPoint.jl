6b8d5040004f3eb59d4bd2b9a950fa138aefe24e
Toby Farley <tobyfarley@gmail.com>
2025-04-29 13:28:19 -0400

    Ints don't have maxintfloat

--- src/BigFixedPoint.jl
+++ src/BigFixedPoint.jl
@@ -69,10 +69,7 @@ function BigFixedPoint(v::V, p::P) where {V<:BigFloat,P<:Integer}
 end
 
 function BigFixedPoint(v::V, p::P) where {V<:Integer,P<:Integer}
-    if (v * (10^p)) > maxintfloat(typeof(v))
-        throw(InexactError(BigFixedPoint, typeof(v), v))
-    end
-    BigFixedPoint{BigInt,P}(BigInt(trunc(round(v; digits=p) * (10^p))), Int(p))
+    BigFixedPoint{BigInt,P}(BigInt(v * (10^p)), Int(p))
 end
 
 function string(z::BigFixedPoint; base::Integer=10, pad::Integer=1)
