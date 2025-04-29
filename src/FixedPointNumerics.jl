42892f1e7f1df402b7a59a428e5072687d450f9a
Toby Farley <tobyfarley@gmail.com>
2025-04-29 10:00:23 -0400

    Fixed bad == for FixPoint/BigFixedPoint

--- src/FixedPointNumerics.jl
+++ src/FixedPointNumerics.jl
@@ -5,14 +5,12 @@ export ==
 include("FixedPoint.jl")
 include("BigFixedPoint.jl")
 
-==(z::FixedPoint, w::BigFixedPoint) = function (z,w)
-        (x,y) = scale(BigFixedPoint(BigInt(z.value),z.precision),w)
-        return eq(x,y)
+function BigFixedPoint(z::FixedPoint)
+    return BigFixedPoint(BigInt(z.value), z.precision)  
 end
 
-==(z::BigFixedPoint, w::FixedPoint) = function (z,w)
-    (x,y) = scale(BigFixedPoint(BigInt(w.value),w.precision),z)
-    return eq(x,y)
-end
+==(z::FixedPoint, w::BigFixedPoint) = eq(scale(BigFixedPoint(z),w),w)
+
+==(z::BigFixedPoint, w::FixedPoint) = eq(scale(BigFixedPoint(z),w),w)
 
 end
